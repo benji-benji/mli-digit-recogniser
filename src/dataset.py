276286b4d8deecbe0006dec10fe691b5f75955d0
ben b <benwbethell@gmail.com>
2025-05-18 20:19:20 +0100

    Finished dataset.py get_mnist_dataloaders function

--- src/dataset.py
+++ src/dataset.py
@@ -21,21 +21,45 @@ From torch.utils.data import:
 
 
 def get_mnist_dataloaders(batch_size=128):
-    """ Makes train and test dataloaders
-    
+    """Makes train and test dataloaders
+
     This function downloads MNIST dataset,
     converts images from MNIST dataset into Pytorch tensors,
     loads the training and testing parts ( 60k and 10k respectively)
     creates the two dataloaders and returns them.
-    
+
     Parameters
     ----------
     Batch_size is set to 128, because otherwise dataloaders default to 1
-    
+
     Returns
     ----------
-    (Training Dataloader, Testing Dataloader)  
-    
+    (Training Dataloader, Testing Dataloader)
+
     """
     transform = transforms.Compose([transforms.ToTensor()])
     # transforms input images from PIL format to PyTorch tensors.
+    # uses .Compose from the transforms module to create a transformation pipeline
+
+    # Create datasets:
+
+    train_dataset = datasets.MNIST(
+        root="data/raw", train=True, download=True, transform=transform
+    )
+    test_dataset = datasets.MNIST(
+        root="data/raw", train=False, download=True, transform=transform
+    )
+    # load the two parts of the data set into the project directory data/raw
+    # train=true/false parameter devides into test and train
+    # download param = True means, if the data is not already in the directory it will be downloaded
+
+    # Create dataloaders:
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    # use the Dataloader module to wrap around the datasets, loading in batches of 128
+    # shuffle for the training set to avoid overfitting
+    # leave unshuffled for the testing set for reproduceability
+
+    # return dataloaders:
+    return train_loader, test_loader
