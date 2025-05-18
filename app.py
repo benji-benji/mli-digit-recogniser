ce65d450ff2d09399323ae19d12d3511559dea84
ben b <benwbethell@gmail.com>
2025-05-18 19:06:23 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write("Hello world")
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+
+df
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+dataframe2 = pd.DataFrame(
+    np.random.randn(10, 20), columns=("col %d" % i for i in range(20))
+)
+
+st.dataframe(dataframe2.style.highlight_max(axis=0))
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
